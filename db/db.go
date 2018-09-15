d1c5a5fbae9ffb87bf6192ba166745e8641c22fd
kageshiron <kageshiron@gmail.com>
2018-09-16 00:57:17 +0900

    handler

--- db/db.go
+++ db/db.go
@@ -1,4 +1,4 @@
-package main
+package db
 
 import (
 	"fmt"
@@ -6,59 +6,71 @@ import (
 	"time"
 )
 
+// A Request represents a log record of HTTP Request.
 type Request struct {
 	Time time.Time
-	http.Request
+	*http.Request
 }
 
-/// Bin
+///////////////////////////////
 
+// A Bin represents a requests bin box.
 type Bin struct {
 	reqs *[]Request
 }
 
+// WriteLog writes a HTTP Request log
 func (bin Bin) WriteLog(request Request) (err error) {
-	*bin.reqs = append(*bin.reqs, request);
+	*bin.reqs = append(*bin.reqs, request)
 	return nil
 }
 
+// ReadLog returns a log
 func (bin Bin) ReadLog(no int) (req *Request, err error) {
-	if len(*bin.reqs) > no {
+	if len(*bin.reqs) <= no {
 		return nil, fmt.Errorf("out of range")
 	}
 	return &(*bin.reqs)[no], nil
 }
 
+// ReadLogs returns Http Request Logs
 func (bin Bin) ReadLogs(index int, length int) (requests []Request, err error) {
 	if len(*bin.reqs) > index+length {
 		return nil, fmt.Errorf("out of range")
 	}
 	return (*bin.reqs)[index : index+length], nil
 }
+
+// Length returns the length of the log records
 func (bin Bin) Length() int {
 	return len(*(bin.reqs))
 }
 
-//////////
+//////////////////
 
+// A BinManager managements bins
 type BinManager struct {
 	save map[string]*Bin
 }
 
+// NewBinManager returns a pointer of a new BinManager
 func NewBinManager() *BinManager {
-	return &BinManager{save:make(map[string]*Bin)}
+	return &BinManager{save: make(map[string]*Bin)}
 }
 
+// Create return new Bin object.
 func (man BinManager) Create(binID string) *Bin {
 	_, ok := man.save[binID]
 	if ok {
 		return nil
 	}
-	b := Bin{}
-	man.save[binID] = new(Bin)
+	r := []Request{}
+	b := Bin{reqs: &r}
+	man.save[binID] = &b
 	return &b
 }
 
+// Bin returns bin which binID pointing
 func (man BinManager) Bin(binID string) *Bin {
 	b, ok := man.save[binID]
 	if !ok {
--- main.go
+++ main.go
@@ -1,37 +1,92 @@
 package main
 
 import (
+	"./db"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
-var man *BinManager= NewBinManager()
+var man *db.BinManager = db.NewBinManager()
 
+// CreateHandler is a handler of (POST) /v1/{id}/create
 func CreateHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	w.WriteHeader(200)
 	v, ok := vars["id"]
 	if !ok {
 		log.Println("create error")
+		w.WriteHeader(400)
+		fmt.Fprint(w, "error")
 	}
 
 	if bin := man.Create(v); bin == nil {
+		w.WriteHeader(500)
 		fmt.Fprint(w, "error")
+
 	} else {
-		fmt.Fprint(w, "success "+v)
+		w.WriteHeader(200)
+		fmt.Fprint(w, "success create "+v)
 	}
 }
-func ItemsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Items!"))
-	log.Println(r.Method)
 
+// ItemsHandler is a handler of (GET) /v1/{id}/items/{num}
+func ItemsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	v, ok := vars["id"]
+	if !ok {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "bad id")
+	}
+
+	num, ok := vars["num"]
+	if !ok {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Bad index")
+	}
+
+	index, err := strconv.Atoi(num)
+	if err != nil {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Bad index")
+		return
+	}
+
+	bin := man.Bin(v)
+	if bin == nil {
+		w.WriteHeader(404)
+		fmt.Fprint(w, "No bin")
+		return
+	}
+
+	res, err := bin.ReadLog(index)
+	if err != nil {
+		fmt.Fprint(w, "No log")
+		return
+	}
+
+	fmt.Fprint(w, res)
 }
+
+// InHandler is a handler of (ANY) /v1/id/in/*
 func InHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello!"))
-	log.Println(r.Method)
+	vars := mux.Vars(r)
+	bin := man.Bin(vars["id"])
+	if bin == nil {
+		w.WriteHeader(404)
+
+		w.Write([]byte("BadRequest"))
+		println(vars["id"])
+		return
+	}
+	w.WriteHeader(200)
+	bin.WriteLog(db.Request{Time: time.Now(), Request: r})
+
+	w.Write([]byte(r.RemoteAddr))
+	println(vars["id"])
 }
 
 func main() {
