bdc232d7ab17a9176c9771fc973f6de2541199d1
kageshiron@gmail.com <kageshiron@gmail.com>
2018-11-19 22:16:33 +0900

    Basic form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,32 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from "react";
+import { Button, Input, Menu } from "semantic-ui-react";
+import Container from "semantic-ui-react/dist/commonjs/elements/Container/Container";
+import "./App.css";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Menu size="large">
+          <Container>
+            <Menu.Item as="span" active={true}>
+              reqhack
+            </Menu.Item>
+            <Menu.Item as="a" active={true}>
+              Home
+            </Menu.Item>
+            <Menu.Item as="a">Work</Menu.Item>
+            <Menu.Item as="a">Company</Menu.Item>
+            <Menu.Item as="a">Careers</Menu.Item>
+            <Menu.Item position="right">Login</Menu.Item>
+          </Container>
+        </Menu>
+        <div>
+          https://<Input icon={{ name: 'redo', circular: true, link: true }} placeholder='bin name' />.reqhack00.esora.xyz
+        </div>
+        <div>
+          <Button> Create New Bin</Button>
+        </div>
       </div>
     );
   }
