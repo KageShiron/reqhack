5f737ea7091816ddc5459ec2508b9e7065a659d4
kageshiron <kageshiron@gmail.com>
2018-12-02 01:45:07 +0900

    Fix: build client & no ssl mode

---
+++ client/make_nginx_config.js
@@ -0,0 +1,62 @@
+const host = process.env.REQHACK_BASEHOST
+if (!host) {
+  console.error("There is't REQHACK_BASEHOST")
+  process.exit(1)
+}
+const random = process.env.REQHACK_RANDOM
+const usessl = process.env.REQHACK_USE_SSL
+
+console.log(`upstream reqhack {
+      server reqhack-server:8081;
+  }
+
+  resolver 127.0.0.1;
+  access_log /var/log/nginx/access2.log;
+  error_log /var/log/nginx/error2.log;
+
+  server {
+      server_name ${host};
+      listen 80 default_server;
+      ${
+        usessl
+          ? `
+      listen 443 ssl default_server;
+      ssl_certificate /etc/nginx/ssl/cert.crt;
+      ssl_certificate_key /etc/nginx/ssl/cert.prv;
+      ssl_protocols        SSLv3 TLSv1;
+      ssl_ciphers          HIGH:!ADH:!MD5;
+      `
+          : ''
+      }
+      location /v1/ {
+          proxy_pass http://reqhack;
+      }
+
+      location / {
+          root /var/www/app;
+          try_files $uri /index.html;
+      }
+  }
+
+  server {
+      server_name ~^(?<subdomain>.*?)\\.${host.replace(/\\./, '\\\\.')};
+      listen 80;
+      ${
+        usessl
+          ? `
+      listen 443 ssl;
+      ssl_certificate /etc/nginx/ssl/cert.crt;
+      ssl_certificate_key /etc/nginx/ssl/cert.prv;
+      ssl_protocols        SSLv3 TLSv1;
+      ssl_ciphers          HIGH:!ADH:!MD5;
+      `
+          : ''
+      }
+      location / {
+          rewrite /(.*) /v1/$subdomain/in/$1 break;
+          proxy_set_header X-Reqhack-Real-IP-${random} $remote_addr;
+          proxy_set_header Host $http_host;
+          proxy_pass http://reqhack;
+      }
+  }
+`)
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
+if [ -z "$REQHACK_BASEHOST" ]; then
+    echo "REQHACK_BASEHOST is empty. use localhost."
+    export REQHACK_BASEHOST="localhost"
+fi
 export REQHACK_RANDOM=$(od -vAn -tx8 -N8 < /dev/random | tr -d " ");
 docker-compose up
