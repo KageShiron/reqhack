cffdae5fc011c290511cccb5aa5b72838cb78807
kageshiron <kageshiron@gmail.com>
2018-12-16 23:10:38 +0900

    move nuxt

--- client/make_nginx_config.js
+++ client/make_nginx_config.js
@@ -18,16 +18,16 @@ console.log(`upstream reqhack {
       server_name ${host};
       listen 80 default_server;
       ${
-        usessl
-          ? `
+  usessl
+    ? `
       listen 443 ssl default_server;
       ssl_certificate /etc/nginx/ssl/cert.crt;
       ssl_certificate_key /etc/nginx/ssl/cert.prv;
       ssl_protocols        SSLv3 TLSv1;
       ssl_ciphers          HIGH:!ADH:!MD5;
       `
-          : ''
-      }
+    : ''
+  }
       location /v1/ {
           proxy_pass http://reqhack;
       }
@@ -42,24 +42,26 @@ console.log(`upstream reqhack {
       server_name ~^(?<subdomain>.*?)\.${host.replace(/\./, '\\.')};
       listen 80;
       ${
-        usessl
-          ? `
+  usessl
+    ? `
       listen 443 ssl;
       ssl_certificate /etc/nginx/ssl/cert.crt;
       ssl_certificate_key /etc/nginx/ssl/cert.prv;
       ssl_protocols        SSLv3 TLSv1;
       ssl_ciphers          HIGH:!ADH:!MD5;
       `
-          : ''
-      }
+    : ''
+  }
       location / {
-          set_by_lua_block $reqhack_raw_request { ngx.req.raw_header() }
+          set_by_lua_block $reqhack_raw_request {
+            local b64 = require("ngx.base64")
+            return b64.encode_base64url( ngx.req.raw_header() )
+          }
           rewrite /(.*) /v1/$subdomain/in/$1 break;
           proxy_set_header X-Reqhack-Real-IP-${random} $remote_addr;
           proxy_set_header X-Reqhack-Real-Port-${random} $remote_port;
-          proxy_set_header X-Reqhack-Real-User-${random} $remote_port;
+          proxy_set_header X-Reqhack-Real-User-${random} $remote_user;
           proxy_set_header X-Reqhack-Real-Scheme-${random} $scheme;
-          proxy_set_header X-Reqhack-Real-Request-${random} $scheme;
           proxy_set_header X-Reqhack-Real-Request-${random} $reqhack_raw_request;
           proxy_set_header Host $http_host;
           proxy_pass http://reqhack;
