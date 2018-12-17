3bfbd1faa3908bf548de820a2d54762b0dc018d9
kageshiron <kageshiron@gmail.com>
2018-12-18 04:41:52 +0900

    fix mysql error

--- server/autodelete/autodelete.sh
+++ server/autodelete/autodelete.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 mkdir -p /var/log/backup
-mysqldump -h database -uroot -pmysql reqhack | bzip2 -9 > /var/log/backup/`date "+%Y%m%d_%H%M%S"`
- < autodelete.sqlmysql -uroot -pmysql -h database
+h databasemysqldump -h database -uroot -pmysql reqhack | bzip2 -9 > /var/log/backup/`date "+%Y%m%d_%H%M%S"`
+                                                                                                                                          < autodelete.sqlmysql -uroot -pmysql -
