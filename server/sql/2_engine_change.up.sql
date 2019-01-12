1103e742431d0953a3e1930c4c92b67889246145
kageshiron <kageshiron@gmail.com>
2019-01-12 20:50:13 +0900

    fix: Primary key

--- server/sql/2_engine_change.up.sql
+++ server/sql/2_engine_change.up.sql
@@ -1,3 +1,4 @@
 ALTER TABLE request drop foreign key request_ibfk_1;
 ALTER TABLE request ENGINE=MyISAM;
 ALTER TABLE request add constraint binid FOREIGN KEY (bin) REFERENCES bin(id);
+ALTER TABLE request DROP PRIMARY KEY, ADD PRIMARY KEY (bin,id);
