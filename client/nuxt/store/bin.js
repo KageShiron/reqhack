d2da8762adead9ad925232f3f86ee5be1d6e22f4
kageshiron@gmail.com <kageshiron@gmail.com>
2019-01-14 18:36:00 +0900

    secret to localStorage

--- client/nuxt/store/bin.js
+++ client/nuxt/store/bin.js
@@ -7,8 +7,17 @@ export const state = () => ({
 
 export const mutations = {
   update_bin(state, value) {
-    if(!value.name) throw "Bad update bin";
-    state.items[value.name] = Object.assign(value.data,state.items[value.name])
+    if (!value.name) throw 'Bad update bin'
+    state.items[value.name] = Object.assign(
+      {},
+      state.items[value.name],
+      value.data
+    )
+    if (value.data.secret) {
+      let secrets = JSON.parse(localStorage.getItem('secrets') || '{}')
+      secrets[value.name] = value.data.secret
+      localStorage.setItem('secrets', JSON.stringify(secrets))
+    }
   },
   update_headerActiveTab(state, value) {
     state.headerActiveTab = value
@@ -17,10 +26,19 @@ export const mutations = {
 
 export const actions = {
   async fetch_bin(context, params) {
-    const secret = params.secret || ( context.state.items[params.name] && context.state.items[params.name].secret) || ""
+    const lsSecret = JSON.parse(localStorage.getItem('secrets') || '{}')[
+      params.name
+    ]
+    const secret =
+      params.secret ||
+      (context.state.items[params.name] &&
+        context.state.items[params.name].secret) ||
+      lsSecret ||
+      ''
     const { data } = await axios.get(
       `/v1/${params.name}/items?secret=${secret}`
     )
+    console.log(data)
     context.commit('update_bin', {
       name: params.name,
       data: { data: data.reverse() }
